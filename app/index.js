61521c8154402eedce286e7c56e8c68b02933362
Isaac Mboussi <117460238+IsaacMboussi@users.noreply.github.com>
2023-07-11 15:38:14 -0700

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView, ActivityIndicator } from "react-native";
+import { useRouter } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { COLORS, icons, image, SIZES } from "../constants";
+import { Welcome, PopularItems } from "../components"
+
+const Home = () => {
+
+  const router = useRouter();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <StatusBar />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome />
+
+          <PopularItems />
+
+          <PopularItems />
+
+         
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
